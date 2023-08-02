c644061476ea416c159efec90ebaeb8d3729c229
Max Matt <77360455+Vsemhoy@users.noreply.github.com>
2023-08-02 12:56:53 -0700

    Added starter page
    Some folder structure and one file

---
+++ resources/views/public/apps/eventor/index.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>A new Super Laravel App</title>
+
+    <!-- Add your CSS files here -->
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <!-- Add more CSS files if needed -->
+</head>
+<body>
+
+    <!-- Your page content goes here -->
+    <div class="container">
+        <h1>Welcome to new hour!</h1>
+        <p>This is hello page. Blabla...</p>
+    </div>
+
+    <!-- Add your JavaScript files here -->
+    <script src="{{ asset('js/app.js') }}"></script>
+    <!-- Add more JavaScript files if needed -->
+</body>
+</html>
